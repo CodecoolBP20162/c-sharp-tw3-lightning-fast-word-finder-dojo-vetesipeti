11b9adf2701f8e619095565202ee26ee20c9be85
vetesipeti <vetesipeti@gmail.com>
2017-07-12 11:02:16 +0200

    Done

--- LightningFastWordFinder/LightningWordFinder.cs
+++ LightningFastWordFinder/LightningWordFinder.cs
@@ -7,10 +7,8 @@ namespace LightningFastWordFinder
     {
         public string GetLongestWord(string text)
         {
-            string[] separators = { ", ", ". ", "!", "?", ";", ": ", " ", "-", "/", "//", "/t" };
+            string[] separators = { ", ", ". ", "!", "?", ";", ": ", " ", "-", "/", "//", "\t", "\n", "\n\r", "(", ")", "www" };
             string[] words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-
-            //string[] words = text.Split(new Char[] { ' ', ',', '.', ';', '!', '?', '\t', '\n', '-' });
             string longest = "";
 
             foreach(string word in words)
@@ -20,28 +18,7 @@ namespace LightningFastWordFinder
             Console.WriteLine(longest);
             return longest;
 
-           /* var currentLargestString = "";
-            string pattern = @"(\w+)\s";
-            foreach (Match m in Regex.Matches(text, pattern))
-            {
-                if (m.Groups[1].Value.Length > currentLargestString.Length) currentLargestString = m.Groups[1].Value;
-            }
-
-          /*      var regex = new Regex(@"(\w+)\s", RegexOptions.Compiled);
-            var match = regex.Match(text);
            
-
-            while (match.Success)
-            {
-                if (match.Groups[1].Value.Length > currentLargestString.Length)
-                {
-                    currentLargestString = match.Groups[1].Value;
-                }
-
-                match = match.NextMatch();
-            }*/
-
-            /*return currentLargestString;*/
         }
     }
 }
